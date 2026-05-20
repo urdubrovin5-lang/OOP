1c0b7aa054a83657a5bb91568e00fd0759297fa9
urdub <urdub@DESKTOP-77QT7U3>
2026-05-20 15:35:05 +0400

    lab4

---
+++ lab4/lab4/ConnectableExtensions.cs
@@ -0,0 +1,10 @@
+﻿public static class ConnectableExtensions
+{
+    public static void ToggleConnection(this IConnectable device)
+    {
+        if (device.IsConnected)
+            device.Disconnect();
+        else
+            device.Connect();
+    }
+}
---
+++ lab4/lab4/IConnectable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+public interface IConnectable
+{
+    bool IsConnected { get; set; }
+    string Name { get; }
+    void Connect();
+    void Disconnect();
+}
