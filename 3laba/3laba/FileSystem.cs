4f90d7306cd0b36abb5b3ebc596a63b2d707c037
urdub <urdub@DESKTOP-77QT7U3>
2026-05-20 15:29:50 +0400

    lab3

---
+++ 3laba/3laba/FileSystem.cs
@@ -0,0 +1,37 @@
+﻿public class FileSystem<T> where T : File, IExecutable
+{
+    private List<T> files = new List<T>();
+    private static int fileSystemCount = 0;
+
+    public FileSystem()
+    {
+        fileSystemCount++;
+    }
+
+    public static int FileSystemCount
+    {
+        get { return fileSystemCount; }
+        set { fileSystemCount = value; }
+    }
+
+    public void AddFile(T file)
+    {
+        files.Add(file);
+        Console.WriteLine($"Файл {file.Name} добавлен");
+    }
+
+    public void ExecuteAll()
+    {
+        Console.WriteLine($"\nВыполнение всех файлов ({files.Count} шт.):");
+        foreach (var file in files)
+        {
+            file.Execute();
+        }
+    }
+
+    public List<T> FindBySize(long maxSize)
+    {
+        return files.Where(f => f.Size <= maxSize).ToList();
+    }
+
+}
---
+++ 3laba/3laba/ProgramFile.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Linq;
+
+public class ProgramFile : File, IExecutable
+{
+    public string Platform { get; set; }
+
+    public ProgramFile(string name, long size, DateTime creationDate, string platform)
+        : base(name, size, creationDate)
+    {
+        Platform = platform;
+    }
+
+    public void Execute()
+    {
+        Console.WriteLine($"Запускаю программу {Name} (платформа: {Platform})");
+    }
+}
